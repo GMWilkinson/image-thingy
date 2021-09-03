c00fdef6277318773458ab0d902204de345ffcdd
Willem B. van der Merwe <iqabsent@gmail.com>
2021-09-04 00:02:41 +0100

    Refactored code .. lots >_<

--- src/App.js
+++ src/App.js
@@ -25,118 +25,78 @@ function App() {
     canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height)
   }
 
+  const effect = {
+    'green': (r, g, b, a) => `rgba(${r}, ${150}, ${b}, ${a})`,
+    'blue': (r, g, b, a) => `rgba(${r}, ${g}, ${150}, ${a})`,
+    'red': (r, g, b, a) => `rgba(${150}, ${g}, ${b}, ${a})`,
+    'r-green': (r, g, b, a) => `rgba(${r}, ${255 - g}, ${b}, ${a})`,
+    'r-blue': (r, g, b, a) => `rgba(${r}, ${g}, ${255 - b}, ${a})`,
+    'r-red': (r, g, b, a) => `rgba(${255 - r}, ${g}, ${b}, ${a})`,
+    'mix': (r, g, b, a) => `rgba(${g}, ${b}, ${r}, ${a})`,
+    'mix-back': (r, g, b, a) => `rgba(${b}, ${g}, ${r}, ${a})`,
+    'step1': (r, g, b, a) => `rgba(${b}, ${r}, ${g}, ${a})`,
+    'step2': (r, g, b, a) => `rgba(${g}, ${b}, ${r}, ${a})`,
+    'b-w1': (r, g, b, a) => `rgba(${r}, ${r}, ${r}, ${a})`,
+    'b-w2': (r, g, b, a) => `rgba(${g}, ${g}, ${g}, ${a})`,
+    'b-w3': (r, g, b, a) => `rgba(${b}, ${b}, ${b}, ${a})`,
+    'opposite': (r, g, b, a) => `rgba(${255 - g}, ${255 - b}, ${255 - r}, ${a})`,
+    'standard': (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a})`,
+    'default': (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a})`,
+    'weirdy': (r, g, b, a) => {
+      const min = Math.min(r, g, b)
+      if (min > 180) return `white`
+      if (min > 150) return 'papayawhip'
+      if (min > 130) return 'yellow'
+      if (min > 100) return 'orange'
+      if (min > 80) return 'mauve'
+      if (min > 60) return 'purple'
+      if (min > 40) return '#2f026e'
+      if (min > 20) return 'black'
+      return 'blue'
+    },
+    'k-bots': (r, g, b, a) => {
+      if (r > 180 && g > 180 && b > 180) return `black`
+      if (r > 150 && g > 150 && b > 150) return 'papayawhip'
+      if (r > 130 && g > 130 && b > 130) return 'purple'
+      if (r > 100 && g > 100 && b > 100) return 'yellow'
+      if (r > 80 && g > 80 && b > 80) return 'mauve'
+      if (r > 60 && g > 60 && b > 60) return 'orange'
+      if (r > 40 && g > 40 && b > 40) return 'red'
+      if (r > 20 && g > 20 && b > 20) return 'white'
+      return 'blue'
+    },
+    'greeny': (r, g, b, a) => {
+      if (r > 220 && g > 220 && b > 220) return `white`
+      if (r > 200 && g > 200 && b > 200) return `#e3fce1`
+      if (r > 180 && g > 180 && b > 180) return `#ccfac8`
+      if (r > 150 && g > 150 && b > 150) return '#a8f7a1'
+      if (r > 130 && g > 130 && b > 130) return '#73f268'
+      if (r > 100 && g > 100 && b > 100) return '#56db4b'
+      if (r > 80 && g > 80 && b > 80) return '#42c437'
+      if (r > 60 && g > 60 && b > 60) return '#24991a'
+      if (r > 40 && g > 40 && b > 40) return '#0f7a06'
+      if (r > 20 && g > 20 && b > 20) return '#063002'
+      return '#18f705'
+    }
+  }
+
   const getPixel = () => {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
+    const pixelData = ctx.getImageData(1,1,width, height).data
     const pixelArray = []
+    let numberOfPixels = width * height
     console.log('start')
-    for (let i = 1; i <= height; i++) {
-      for (let j = 1; j <= width; j++) {
-        const [r, g, b, a] = ctx.getImageData(j, i, 1, 1).data
-        let colorString
-        if (type === 'green') {
-          colorString = `rgba(${r}, 150, ${b}, ${a})`
-        } else if (type === 'blue') {
-          colorString = `rgba(${r}, ${g}, 150, ${a})`
-        } else if (type === 'red') {
-          colorString = `rgba(150, ${g}, ${b}, ${a})`
-        } else if (type === 'r-green') {
-          colorString = `rgba(${r}, ${255 - g}, ${b}, ${a})`
-        } else if (type === 'r-blue') {
-          colorString = `rgba(${r}, ${g}, ${255 - b}, ${a})`
-        } else if (type === 'r-red') {
-          colorString = `rgba(${255 - r}, ${g}, ${b}, ${a})`
-        } else if (type === 'mix') {
-          colorString = `rgba(${g}, ${b}, ${r}, ${a})`
-        } else if (type === 'mix-back') {
-          colorString = `rgba(${b}, ${g}, ${r}, ${a})`
-        } else if (type === 'step1') {
-          colorString = `rgba(${b}, ${r}, ${g}, ${a})`
-        } else if (type === 'step2') {
-          colorString = `rgba(${g}, ${b}, ${r}, ${a})`
-        } else if (type === 'weirdy') {
-          if (r > 180 && g > 180 && b > 180) {
-            colorString = `white`
-          } else if (r > 150 && g > 150 && b > 150) {
-            colorString = 'papayawhip'
-          } else if (r > 130 && g > 130 && b > 130) {
-            colorString = 'yellow'
-          } else if (r > 100 && g > 100 && b > 100) {
-            colorString = 'orange'
-          } else if (r > 80 && g > 80 && b > 80) {
-            colorString = 'mauve'
-          } else if (r > 60 && g > 60 && b > 60) {
-            colorString = 'purple'
-          } else if (r > 40 && g > 40 && b > 40) {
-            colorString = '#2f026e'
-          } else if (r > 20 && g > 20 && b > 20) {
-            colorString = 'black'
-          }  else {
-            colorString = 'blue'
-          }
-        } else if (type === 'k-bots') {
-          if (r > 180 && g > 180 && b > 180) {
-            colorString = `black`
-          } else if (r > 150 && g > 150 && b > 150) {
-            colorString = 'papayawhip'
-          } else if (r > 130 && g > 130 && b > 130) {
-            colorString = 'purple'
-          } else if (r > 100 && g > 100 && b > 100) {
-            colorString = 'yellow'
-          } else if (r > 80 && g > 80 && b > 80) {
-            colorString = 'mauve'
-          } else if (r > 60 && g > 60 && b > 60) {
-            colorString = 'orange'
-          } else if (r > 40 && g > 40 && b > 40) {
-            colorString = 'red'
-          } else if (r > 20 && g > 20 && b > 20) {
-            colorString = 'white'
-          }  else {
-            colorString = 'blue'
-          }
-        } else if (type === 'greeny') {
-          if (r > 220 && g > 220 && b > 220) {
-            colorString = `white`
-          } else if (r > 200 && g > 200 && b > 200) {
-            colorString = `#e3fce1`
-          } else if (r > 180 && g > 180 && b > 180) {
-            colorString = `#ccfac8`
-          } else if (r > 150 && g > 150 && b > 150) {
-            colorString = '#a8f7a1'
-          } else if (r > 130 && g > 130 && b > 130) {
-            colorString = '#73f268'
-          } else if (r > 100 && g > 100 && b > 100) {
-            colorString = '#56db4b'
-          } else if (r > 80 && g > 80 && b > 80) {
-            colorString = '#42c437'
-          } else if (r > 60 && g > 60 && b > 60) {
-            colorString = '#24991a'
-          } else if (r > 40 && g > 40 && b > 40) {
-            colorString = '#0f7a06'
-          } else if (r > 20 && g > 20 && b > 20) {
-            colorString = '#063002'
-          }  else {
-            colorString = '#18f705'
-          }
-        } else if (type === 'b-w1') {
-          colorString = `rgba(${r}, ${r}, ${r}, ${a})`
-        } else if (type === 'b-w2') {
-          colorString = `rgba(${g}, ${g}, ${g}, ${a})`
-        } else if (type === 'b-w3') {
-          colorString = `rgba(${b}, ${b}, ${b}, ${a})`
-        } else if (type === 'opposite') {
-          colorString = `rgba(${255 - g}, ${255 - b}, ${255 - r}, ${a})`
-        } else if (type === 'standard') {
-          colorString = `rgba(${r}, ${g}, ${b}, ${a})`
-        } else {
-          colorString = `rgba(${r}, ${g}, ${b}, ${a})`
-        }
-        if (a !== 0) {
-          pixelArray.push(<div style={{width: 1, height: 1, backgroundColor: colorString}}></div> )
-        } else {
-          pixelArray.push(<div style={{width: 1, height: 1, backgroundColor: 'black'}}></div> )
-        }
+    for (numberOfPixels; numberOfPixels; numberOfPixels--) {
+      const [r, g, b, a] = pixelData.slice((numberOfPixels - 1) * 4, numberOfPixels * 4)
+      let colorString
+      if (a === 0) {
+        colorString = 'black'
+      } else {
+        const effectFunction = effect[type] || effect.default
+        colorString = effectFunction(r, g, b, a)
       }
+      pixelArray.unshift(<div style={{width: 1, height: 1, backgroundColor: colorString}}></div> )
     }
     console.log('finish')
     setPixels(pixelArray)
@@ -147,7 +107,7 @@ function App() {
       <main className="App-header">
         <img 
           id="my-image" 
-          src={process.env.PUBLIC_URL + '/strad.jpg'} 
+          src={process.env.PUBLIC_URL + '/400.jpg'} 
         />
         <div className="box">
           {drawnImage && drawnImage.map((pixel) => 
