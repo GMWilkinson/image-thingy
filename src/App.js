3f8506e43be91ef5078495c8b48d480f2f3bea5d
Willem B. van der Merwe <iqabsent@gmail.com>
2021-09-04 19:29:35 +0100

    Make named colors use rgba

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { fireEvent } from '@testing-library/react';
 import React, {useState, useEffect} from 'react'
 import './App.css'
 
@@ -34,65 +35,104 @@ function App() {
     box.style = `width: ${img.width}px; height: ${img.height}px`
   }
 
+  const nameToRgba = (name) => {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+    canvas.width = 1;
+    canvas.height = 1;
+    context.fillStyle = name;
+    context.fillRect(0,0,1,1);
+    return [...context.getImageData(0,0,1,1).data];
+  }
+
+  const namedColors = {
+    "white":[255,255,255,255],
+    "papayawhip":[255,239,213,255],
+    "yellow":[255,255,0,255],
+    "orange":[255,165,0,255],
+    "mauve":[0,0,0,255],
+    "purple":[128,0,128,255],
+    "#2f026e":[47,2,110,255],
+    "black":[0,0,0,255],
+    "blue":[0,0,255,255],
+    "red":[255,0,0,255],
+    "#e3fce1":[227,252,225,255],
+    "#ccfac8":[204,250,200,255],
+    "#a8f7a1":[168,247,161,255],
+    "#73f268":[115,242,104,255],
+    "#56db4b":[86,219,75,255],
+    "#42c437":[66,196,55,255],
+    "#24991a":[36,153,26,255],
+    "#0f7a06":[15,122,6,255],
+    "#063002":[6,48,2,255],
+    "#18f705":[24,247,5,255]
+  }
+  // for (const name in namedColors) {
+  //   namedColors[name] = nameToRgba(name)
+  // }
+  // console.log(namedColors);
+
   const effect = {
-    'green': (r, g, b, a) => `rgba(${r}, ${150}, ${b}, ${a})`,
-    'blue': (r, g, b, a) => `rgba(${r}, ${g}, ${150}, ${a})`,
-    'red': (r, g, b, a) => `rgba(${150}, ${g}, ${b}, ${a})`,
-    'r-green': (r, g, b, a) => `rgba(${r}, ${255 - g}, ${b}, ${a})`,
-    'r-blue': (r, g, b, a) => `rgba(${r}, ${g}, ${255 - b}, ${a})`,
-    'r-red': (r, g, b, a) => `rgba(${255 - r}, ${g}, ${b}, ${a})`,
-    'mix': (r, g, b, a) => `rgba(${g}, ${b}, ${r}, ${a})`,
-    'mix-back': (r, g, b, a) => `rgba(${b}, ${g}, ${r}, ${a})`,
-    'step1': (r, g, b, a) => `rgba(${b}, ${r}, ${g}, ${a})`,
-    'step2': (r, g, b, a) => `rgba(${g}, ${b}, ${r}, ${a})`,
-    'b-w1': (r, g, b, a) => `rgba(${r}, ${r}, ${r}, ${a})`,
-    'b-w2': (r, g, b, a) => `rgba(${g}, ${g}, ${g}, ${a})`,
-    'b-w3': (r, g, b, a) => `rgba(${b}, ${b}, ${b}, ${a})`,
-    'opposite': (r, g, b, a) => `rgba(${255 - g}, ${255 - b}, ${255 - r}, ${a})`,
-    'standard': (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a})`,
-    'default': (r, g, b, a) => `rgba(${r}, ${g}, ${b}, ${a})`,
+    'green': (r, g, b, a) => [r, 150, b, a],
+    'blue': (r, g, b, a) => [r, g, 150, a],
+    'red': (r, g, b, a) => [150, g, b, a],
+    'r-green': (r, g, b, a) => [r, 255 - g, b, a],
+    'r-blue': (r, g, b, a) => [r, g, 255 - b, a],
+    'r-red': (r, g, b, a) => [255 - r, g, b, a],
+    'mix': (r, g, b, a) => [g, b, r, a],
+    'mix-back': (r, g, b, a) => [b, g, r, a],
+    'step1': (r, g, b, a) => [b, r, g, a],
+    'step2': (r, g, b, a) => [g, b, r, a],
+    'b-w1': (r, g, b, a) => [r, r, r, a],
+    'b-w2': (r, g, b, a) => [g, g, g, a],
+    'b-w3': (r, g, b, a) => [b, b, b, a],
+    'opposite': (r, g, b, a) => [255 - g, 255 - b, 255 - r, a],
+    'standard': (r, g, b, a) => [r, g, b, a],
+    'default': (r, g, b, a) => [r, g, b, a],
     'weirdy': (r, g, b, a) => {
       const min = Math.min(r, g, b)
-      if (min > 180) return `white`
-      if (min > 150) return 'papayawhip'
-      if (min > 130) return 'yellow'
-      if (min > 100) return 'orange'
-      if (min > 80) return 'mauve'
-      if (min > 60) return 'purple'
-      if (min > 40) return '#2f026e'
-      if (min > 20) return 'black'
-      return 'blue'
+      return (min > 180) ? namedColors['white']
+        : (min > 150) ? namedColors['papayawhip']
+        : (min > 130) ? namedColors['yellow']
+        : (min > 100) ? namedColors['orange']
+        : (min > 80) ? namedColors['mauve']
+        : (min > 60) ? namedColors['purple']
+        : (min > 40) ? namedColors['#2f026e']
+        : (min > 20) ? namedColors['black']
+        : namedColors['blue']
     },
     'k-bots': (r, g, b, a) => {
-      if (r > 180 && g > 180 && b > 180) return `black`
-      if (r > 150 && g > 150 && b > 150) return 'papayawhip'
-      if (r > 130 && g > 130 && b > 130) return 'purple'
-      if (r > 100 && g > 100 && b > 100) return 'yellow'
-      if (r > 80 && g > 80 && b > 80) return 'mauve'
-      if (r > 60 && g > 60 && b > 60) return 'orange'
-      if (r > 40 && g > 40 && b > 40) return 'red'
-      if (r > 20 && g > 20 && b > 20) return 'white'
-      return 'blue'
+      const min = Math.min(r, g, b)
+      if (min > 180) return namedColors['black']
+      if (min > 150) return namedColors['papayawhip']
+      if (min > 130) return namedColors['purple']
+      if (min > 100) return namedColors['yellow']
+      if (min > 80) return namedColors['mauve']
+      if (min > 60) return namedColors['orange']
+      if (min > 40) return namedColors['red']
+      if (min > 20) return namedColors['white']
+      return namedColors['blue']
     },
     'greeny': (r, g, b, a) => {
-      if (r > 220 && g > 220 && b > 220) return `white`
-      if (r > 200 && g > 200 && b > 200) return `#e3fce1`
-      if (r > 180 && g > 180 && b > 180) return `#ccfac8`
-      if (r > 150 && g > 150 && b > 150) return '#a8f7a1'
-      if (r > 130 && g > 130 && b > 130) return '#73f268'
-      if (r > 100 && g > 100 && b > 100) return '#56db4b'
-      if (r > 80 && g > 80 && b > 80) return '#42c437'
-      if (r > 60 && g > 60 && b > 60) return '#24991a'
-      if (r > 40 && g > 40 && b > 40) return '#0f7a06'
-      if (r > 20 && g > 20 && b > 20) return '#063002'
-      return '#18f705'
+      const min = Math.min(r, g, b)
+      if (min > 220) return namedColors['white']
+      if (min > 200) return namedColors['#e3fce1']
+      if (min > 180) return namedColors['#ccfac8']
+      if (min > 150) return namedColors['#a8f7a1']
+      if (min > 130) return namedColors['#73f268']
+      if (min > 100) return namedColors['#56db4b']
+      if (min > 80) return namedColors['#42c437']
+      if (min > 60) return namedColors['#24991a']
+      if (min > 40) return namedColors['#0f7a06']
+      if (min > 20) return namedColors['#063002']
+      return namedColors['#18f705']
     }
   }
 
   const getPixel = () => {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
-    const pixelData = ctx.getImageData(1,1,width, height).data
+    const pixelData = ctx.getImageData(0,0,width, height).data
     const pixelArray = []
     let numberOfPixels = width * height
     console.log('start')
@@ -103,13 +143,16 @@ function App() {
         colorString = 'black'
       } else {
         const effectFunction = effect[type] || effect.default
-        colorString = effectFunction(r, g, b, a)
+        const convertedRgba = effectFunction(r, g, b, a)
+        colorString = toColorString(convertedRgba)
       }
       pixelArray.unshift(<div style={{width: pixelScale, height: pixelScale, backgroundColor: colorString}}></div> )
     }
     console.log('finish')
     setPixels(pixelArray)
   }
+
+  const toColorString = (rgba) => `rgba(${rgba.join()})`
 
   return (
     <div className="App">
